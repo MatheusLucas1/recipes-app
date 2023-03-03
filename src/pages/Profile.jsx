90220d8c48a82e178d5e9470f224c3f723bccf1d
Victor Massini <vmprata@gmail.com>
2023-03-03 15:08:37 -0300

    iniciando o profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 export default function Profile() {
   return (
     <div>
-      Profile
+      Profileeeeee
       <Header title="Profile" showSearch={ false } />
     </div>
   );
